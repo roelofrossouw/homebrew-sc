7b03a626adcb14e405d4603cfbebcec4d7391c86
Roelof Rossouw <roelof@1web.co.za>
2026-09-18 17:24:18 +0200

    Updated model distro naming and scripts

---
+++ Formula/simply-cpp-db.rb
@@ -0,0 +1,15 @@
+class SimplyCppDb < Formula
+    desc "C++20 wrapper around libpq for PostgreSQL, other databases to follow"
+    homepage "https://github.com/roelofrossouw/simply-cpp-db"
+    url "https://github.com/roelofrossouw/simply-cpp-db/archive/refs/tags/v1.0.4.tar.gz"
+    sha256 "86e7b2fa74c6309b3c9b4bdb3e1829aab93bdc229ad0a6eff451d3140c92a46a"
+    license "MIT"
+
+    depends_on "cmake" => :build
+
+    def install
+        system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+        system "cmake", "--build", "build"
+        system "cmake", "--install", "build"
+    end
+end
---
+++ Formula/simply-cpp-image.rb
@@ -0,0 +1,15 @@
+class SimplyCppImage < Formula
+    desc "C++20 wrapper around OpenCV for loading, saving, and processing images"
+    homepage "https://github.com/roelofrossouw/simply-cpp-image"
+    url "https://github.com/roelofrossouw/simply-cpp-image/archive/refs/tags/v1.0.2.tar.gz"
+    sha256 "a4097ed0c16cdab1c14fc0af66db58c90f43cba64a39f7f9e66f66c0d7d72c80"
+    license "MIT"
+
+    depends_on "cmake" => :build
+
+    def install
+        system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+        system "cmake", "--build", "build"
+        system "cmake", "--install", "build"
+    end
+end
