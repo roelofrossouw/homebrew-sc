e87270132590fb44fcc9925500e349dadf5f7053
Roelof Rossouw <roelof@1web.co.za>
2026-09-18 12:51:48 +0200

    Added homebrew deployment script

--- Formula/simply-cpp.rb
+++ Formula/simply-cpp.rb
@@ -1,16 +1,17 @@
 class SimplyCpp < Formula
-   desc "Simply C++ framework"
-   homepage "https://github.com/roelofrossouw/simply-cpp"
+    desc "Simply C++ framework"
+    homepage "https://github.com/roelofrossouw/simply-cpp"
+    url "https://github.com/roelofrossouw/simply-cpp/archive/refs/tags/v1.1.5.tar.gz"
+    sha256 "0565511508ed4657a866552fa0c8e80064c13b852f2fde7a37f095ecd7fcb94a"
+    license "MIT"
 
-   if Hardware::CPU.arm?
-     url "https://apt.roelof.co.za/hb/macos/simply-cpp-1.1.4-arm64.tar.gz"
-     sha256 "..."
-   else
-     url "https://apt.roelof.co.za/hb/macos/simply-cpp-1.1.4-x86_64.tar.gz"
-     sha256 "..."
-   end
+    depends_on "cmake" => :build
 
-   def install
-     prefix.install Dir["*"]
-   end
- end
+    def install
+        system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+        system "cmake", "--build", "build"
+        system "cmake", "--install", "build"
+    end
+
+end
+
