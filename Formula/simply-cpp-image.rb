02d0a06c4ae09a90d9c9c94d816c40f7d5800cbd
Roelof Rossouw <roelof@1web.co.za>
2026-09-23 14:13:50 +0200

    Bump image to v1.0.4

--- Formula/simply-cpp-image.rb
+++ Formula/simply-cpp-image.rb
@@ -1,8 +1,8 @@
 class SimplyCppImage < Formula
     desc "C++20 wrapper around OpenCV for loading, saving, and processing images"
     homepage "https://github.com/roelofrossouw/simply-cpp-image"
-    url "https://github.com/roelofrossouw/simply-cpp-image/archive/refs/tags/v1.0.3.tar.gz"
-    sha256 "82abded6799a6e2416cd3106d6a6b720308d5b96508fbf35396b6d33f74bf832"
+    url "https://github.com/roelofrossouw/simply-cpp-image/archive/refs/tags/v1.0.4.tar.gz"
+    sha256 "c0bfe2fec947c0579436628e7c5e98214afb7fba1e91425ef36d40cdd0cf4d75"
     license "Apache-2.0"
 
     depends_on "cmake" => :build
