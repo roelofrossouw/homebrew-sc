d69ced565440708b58cf370a3e7c65c8d4935a25
Roelof Rossouw <roelof@1web.co.za>
2026-09-23 13:56:38 +0200

    Bump image to v1.0.3, ai to v1.0.4

--- Formula/simply-cpp-ai.rb
+++ Formula/simply-cpp-ai.rb
@@ -1,8 +1,8 @@
 class SimplyCppAi < Formula
     desc "Wrappers for common AI tooling: ONNX, YOLO, and more"
     homepage "https://github.com/roelofrossouw/simply-cpp-ai"
-    url "https://github.com/roelofrossouw/simply-cpp-ai/archive/refs/tags/v1.0.3.tar.gz"
-    sha256 "4ab2215cc2a9055d2494183de7620050139eee3fa8110ed16112e841d642cd61"
+    url "https://github.com/roelofrossouw/simply-cpp-ai/archive/refs/tags/v1.0.4.tar.gz"
+    sha256 "23b8f22d8de98923f7c5a0a472e505892541da7bcad39417355d8fa41986f255"
     license "Apache-2.0"
 
     depends_on "cmake" => :build
--- Formula/simply-cpp-image.rb
+++ Formula/simply-cpp-image.rb
@@ -1,8 +1,8 @@
 class SimplyCppImage < Formula
     desc "C++20 wrapper around OpenCV for loading, saving, and processing images"
     homepage "https://github.com/roelofrossouw/simply-cpp-image"
-    url "https://github.com/roelofrossouw/simply-cpp-image/archive/refs/tags/v1.0.2.tar.gz"
-    sha256 "a4097ed0c16cdab1c14fc0af66db58c90f43cba64a39f7f9e66f66c0d7d72c80"
+    url "https://github.com/roelofrossouw/simply-cpp-image/archive/refs/tags/v1.0.3.tar.gz"
+    sha256 "82abded6799a6e2416cd3106d6a6b720308d5b96508fbf35396b6d33f74bf832"
     license "Apache-2.0"
 
     depends_on "cmake" => :build
