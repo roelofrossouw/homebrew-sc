24300e505a722a3e5c76ecbb40b2cce3af9ea9de
Roelof Rossouw <roelof@1web.co.za>
2026-09-23 16:10:00 +0200

    Bump core to v1.1.13, ai to v1.0.9

--- Formula/simply-cpp-ai.rb
+++ Formula/simply-cpp-ai.rb
@@ -1,8 +1,8 @@
 class SimplyCppAi < Formula
     desc "Wrappers for common AI tooling: ONNX, YOLO, and more"
     homepage "https://github.com/roelofrossouw/simply-cpp-ai"
-    url "https://github.com/roelofrossouw/simply-cpp-ai/archive/refs/tags/v1.0.8.tar.gz"
-    sha256 "5df616f0aeb4794056f3b0d5101ee5c3bc92b67046041e51f0a6aec3cd88066e"
+    url "https://github.com/roelofrossouw/simply-cpp-ai/archive/refs/tags/v1.0.9.tar.gz"
+    sha256 "e22b8dd1d192272c98e439896ec6e9a967629f56d4340acb568d5ae512429216"
     license "Apache-2.0"
 
     depends_on "cmake" => :build
--- Formula/simply-cpp.rb
+++ Formula/simply-cpp.rb
@@ -1,8 +1,8 @@
 class SimplyCpp < Formula
     desc "Library that wraps common C++ libraries behind one simple, consistent API"
     homepage "https://github.com/roelofrossouw/simply-cpp"
-    url "https://github.com/roelofrossouw/simply-cpp/archive/refs/tags/v1.1.12.tar.gz"
-    sha256 "34587acf67db3a3e6a0745857bbd186894b8eef0df2066cfc543dea3c914c666"
+    url "https://github.com/roelofrossouw/simply-cpp/archive/refs/tags/v1.1.13.tar.gz"
+    sha256 "361fc1584b6a8f265fe2036c3796eba4d178cc961ca44031d01b85ee6f8543e3"
     license "Apache-2.0"
 
     depends_on "cmake" => :build
