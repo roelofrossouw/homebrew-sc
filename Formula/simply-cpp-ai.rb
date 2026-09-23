2e99f0399bb8a4748f5d4638d51e3f0f1e01f3ad
Roelof Rossouw <roelof@1web.co.za>
2026-09-23 14:32:55 +0200

    Bump image to v1.0.5, ai to v1.0.5

--- Formula/simply-cpp-ai.rb
+++ Formula/simply-cpp-ai.rb
@@ -1,8 +1,8 @@
 class SimplyCppAi < Formula
     desc "Wrappers for common AI tooling: ONNX, YOLO, and more"
     homepage "https://github.com/roelofrossouw/simply-cpp-ai"
-    url "https://github.com/roelofrossouw/simply-cpp-ai/archive/refs/tags/v1.0.4.tar.gz"
-    sha256 "23b8f22d8de98923f7c5a0a472e505892541da7bcad39417355d8fa41986f255"
+    url "https://github.com/roelofrossouw/simply-cpp-ai/archive/refs/tags/v1.0.5.tar.gz"
+    sha256 "85eb7e5fc45b82aa8261f48ee508adab50d3a62209468e0161abba26546c0189"
     license "Apache-2.0"
 
     depends_on "cmake" => :build
