d565c113acbde043dc54af3d01743fb77a70ff28
Roelof Rossouw <roelof@1web.co.za>
2026-09-24 14:29:52 +0200

    Bump simply-cpp to v1.2.0

--- Formula/simply-cpp.rb
+++ Formula/simply-cpp.rb
@@ -1,8 +1,8 @@
 class SimplyCpp < Formula
     desc "Library that wraps common C++ libraries behind one simple, consistent API"
     homepage "https://github.com/roelofrossouw/simply-cpp"
-    url "https://github.com/roelofrossouw/simply-cpp/archive/refs/tags/v1.1.18.tar.gz"
-    sha256 "3461108d58d2fb8d4fa5b43eb3b4309ec88276deb2e9a1aaf14845262e97dcad"
+    url "https://github.com/roelofrossouw/simply-cpp/archive/refs/tags/v1.2.0.tar.gz"
+    sha256 "eff785cce809eda0d86cea5555d29e8c16a217344adefa2f216bc4665999958b"
     license "Apache-2.0"
 
     depends_on "cmake" => :build
